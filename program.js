ba2401026f1e2852a6d7c9a6253244241add9c19
karenpeng <itismekarenp@gmail.com>
2014-07-31 18:45:04 -0400

    learning

---
+++ program.js
@@ -0,0 +1,11 @@
+//console.log('BEEP BOOP')
+
+/*
+var uniq = require('uniq')
+console.log(uniq(prompt().split(',')))
+*/
+
+/*
+var uniquely = require('./uniquely.js')
+console.log(uniquely(prompt()))
+*/
---
+++ uniquely.js
@@ -0,0 +1,7 @@
+var uniq = require('uniq')
+
+module.exports = function(xs){
+	
+	 return uniq(xs.split(','))
+
+}
